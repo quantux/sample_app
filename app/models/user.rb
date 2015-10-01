08a5cc4f84be05587defbc6571a7902e7cc7feb4
Matheus Faria <mths.faria@hotmail.com>
2015-10-01 17:32:23 -0300

    Updating user.rb to ensure only valid email addresses get passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+	
+	EMAIL_VALIDATION_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255}
+	validates :email, format: { with: EMAIL_VALIDATION_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,4 +31,11 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
+	test "email validation should accept valid addressess" do
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
 end
