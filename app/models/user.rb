1562e3da1be45b68f79cb3f2b069f71cab654312
Matheus Faria <mths.faria@hotmail.com>
2015-10-03 16:02:34 -0300

    Updating model rules.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	EMAIL_VALIDATION_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, presence: true, length: {maximum: 50}
-	validates :email, presence: true, length: {maximum: 255}
-	validates :email, format: { with: EMAIL_VALIDATION_REGEX }
+	validates :email, presence: true, length: { maximum: 255 }, 
+			  format: { with: EMAIL_VALIDATION_REGEX }, uniqueness: { case_sensitive: false }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,4 +38,13 @@ class UserTest < ActiveSupport::TestCase
 			assert @user.valid?, "#{valid_address.inspect} should be valid"
 		end
 	end
+
+	test "email addressess should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+
 end
