ce36c8b94213534b9640a2763dda3ca7e10769db
Matheus Faria <mths.faria@hotmail.com>
2015-10-05 15:52:12 -0300

    Implementing a Callback function in order to downcase all email addresses.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	
 	EMAIL_VALIDATION_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+	before_save = { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: { maximum: 255 }, 
 			  format: { with: EMAIL_VALIDATION_REGEX }, uniqueness: { case_sensitive: false }
