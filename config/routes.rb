80aea8c2b0bff4355b889e1597703706fcf86900
Matheus Faria <mths.faria@hotmail.com>
2015-10-20 13:21:19 -0200

    Turning Users a resource which can be acessed by /users/1 URL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'signup' => 'users#new'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+
+  get 'signup' => 'users#new'
 
   get 'help' => 'static_pages#help'
 
   get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
+
+  resources :users
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
