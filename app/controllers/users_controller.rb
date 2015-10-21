dbf5c8d01e2466a8aecb9b553dc725770df6c322
Matheus Faria <mths.faria@hotmail.com>
2015-10-21 20:27:58 -0200

    Starting implementation of the crate action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def create
+  	@user = @user.new(params[:user]) # Not the final implementation
+  	if @user.save
+  		# Handle a successful save
+  	else
+
+  	end
+  end
 end
